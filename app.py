e80fc2acde0cc316df28247037eb216de2f95d40
arvinmoj <arvinmojaverian@hotmail.com>
2023-05-26 12:50:17 -0400

    setup langchain and write first sample

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import logging
+import langchain
+from dotenv import load_dotenv
+from langchain.llms import OpenAI
+
+#region - Base Logging -
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+#endregion - Base Logging -
+
+#region - Load Environment Variables -
+load_dotenv()
+OPEN_AI_TOKEN = os.environ.get("OPEN_AI_TOKEN")
+#endregion - Load Environment Variables -
+
+logging.log(logging.INFO, "Starting Langchain")
+
+llm_open_ai = OpenAI(model="text-davinci-003", openai_api_key=OPEN_AI_TOKEN)
+
+user_input = input("Enter your prompt:")
+prompt = f"{user_input}"
+
+completion_open_ai = llm_open_ai(prompt)
+logging.log(logging.INFO, completion_open_ai)
