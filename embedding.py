87eeb4238e9a2f8ec6b001c5bd157c34ab08566a
arvinmoj <arvinmojaverian@hotmail.com>
2023-05-27 15:55:31 -0400

    add opan ai llm competion handler

--- embedding.py
+++ embedding.py
@@ -1,6 +1,5 @@
-import os 
+import llm
 import logging
-from load_dotenv import load_dotenv
 from langchain import PromptTemplate
 
 #region - Base Logging -
@@ -8,20 +7,13 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                      level=logging.INFO)
 #endregion - Base Logging -
 
-
-load_dotenv()
-OPEN_AI_TOKEN = os.environ.get("OPEN_AI_TOKEN")
-os.environ["OPENAI_API_KEY"] = OPEN_AI_TOKEN
-
-
 template = "What is a good name for a company that makes {product}?"
 
 prompt = PromptTemplate(
     input_variables=["product"],
     template=template,
 )
-
-user_input = input("Enter a product: ")
-
-completion_user = prompt.format(product=user_input)
+completion_user = prompt.format(product="computer")
 logging.log(logging.INFO, completion_user)
+
+llm.open_ai_llm_completion_handler(completion_user)
